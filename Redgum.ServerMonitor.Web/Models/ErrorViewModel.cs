1ba0041e7b21bcfab44cb3a79367596c5446e42d
Keith Urquhart <keith@redgumtech.com.au>
2017-12-05 10:05:24 +1100

    Add project files.

---
+++ Redgum.ServerMonitor.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Redgum.ServerMonitor.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
